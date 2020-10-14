837bfef7aa069221920eb70f2e499dc877f13488
Alex Tochetto <alextochetto@hotmail.com>
2020-10-13 22:02:13 -0300

    create component to pkce openidconnect events

---
+++ src/Extensions/Hopper.OpenIdConnect.Security.Pkce/Events/AuthorizationCodeReceived.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+//namespace Hopper.OpenIdConnect.Security.Pkce
+namespace Microsoft.AspNetCore.Authentication.OpenIdConnect
+{
+    public static class AuthorizationCodeReceived
+    {
+        private static Func<AuthorizationCodeReceivedContext, Task> OnAuthorizationCodeReceivedEvent()
+        {
+            return context =>
+            {
+                // only when authorization code is being swapped for tokens
+                if (context.TokenEndpointRequest?.GrantType != OpenIdConnectGrantTypes.AuthorizationCode) 
+                    return Task.CompletedTask;
+                // get stored code_verifier
+                if (context.Properties.Items.TryGetValue("code_verifier", out string codeVerifier))
+                {
+                    // add code_verifier to token request
+                    context.TokenEndpointRequest.Parameters.Add("code_verifier", codeVerifier);
+                }
+                return Task.CompletedTask;
+            };
+        }
+    }
+}
---
+++ src/Extensions/Hopper.OpenIdConnect.Security.Pkce/Events/RedirectToIdentityProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace Hopper.OpenIdConnect.Security.Pkce
+{
+    public static class RedirectToIdentityProvider
+    {
+        // private static Func<RedirectContext, Task> OnRedirectToIdentityProviderEvent()
+        // {
+
+        // }
+    }
+}
